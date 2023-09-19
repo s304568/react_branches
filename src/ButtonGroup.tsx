5958a4e020168c6f09df5bf13ea64f05238b1973
Alex Bowker <alex.bowker-lonnecker@salfordcc.ac.uk>
2023-09-19 23:56:56 +0100

    Add ButtonGroup without state

--- src/ButtonGroup.tsx
+++ src/ButtonGroup.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-
 function ButtonGroup() {
   const buttons = ["Red", "Green", "Blue"];
-  const [buttonText, setButtonText] = useState("");
-
-  const handleButtonClick = (button: string) =>
-    setButtonText(button + " was pressed!");
 
   const buttonTags = buttons.map((button) => (
-    <button
-      type="button"
-      key={button}
-      onClick={() => handleButtonClick(button)}
-    >
+    <button type="button" key={button} onClick={() => console.log(button)}>
       {button}
     </button>
   ));
@@ -20,7 +10,6 @@ function ButtonGroup() {
   return (
     <>
       <div className="flex-container">{buttonTags}</div>
-      <p>{buttonText}</p>
     </>
   );
 }
