0de3e3c148c7775b87f0228205fc89318e9e2eea
Nikola Todorov <s304568@student.salfordcc.ac.uk>
2023-10-06 10:29:02 +0100

    Finish activity 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import DinoImage from "./DinoImage";
+import FunnyImage from "./FunnyImage";
 import Paragraph from "./Paragraph";
+import NewText from "./NewText";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <h1>Hello World!</h1>
       <Paragraph />;
       <DinoImage />
+      <FunnyImage/>
+      <NewText/>
     </>
   );
 }
---
+++ src/FunnyImage.tsx
@@ -0,0 +1,8 @@
+import funnyImage from "./assets/funny_pick.jpg";
+
+function FunnyImage() {
+ return <img src={funnyImage} width="20%"/>;
+
+}
+
+export default FunnyImage;
